dd703bffa188c3cac92de6d73ab3e9fb836f4a4c
StepaOpa <stepankock@yandex.ru>
2026-01-30 02:22:41 +0500

    add channels parsing, logging, database

---
+++ database_manager.py
@@ -0,0 +1,54 @@
+import sqlite3
+from typing import List, Optional, Final
+from datetime import datetime
+import logging
+
+logger = logging.getLogger(__name__)
+
+class DatabaseManager:
+    def __init__(self, db_path: str = "news_archive.db") -> None:
+        self.db_path: Final[str] = db_path
+        self._create_table()
+        logger.info('База данных инициализирована')
+
+    def _create_table(self) -> None:
+        """Создает таблицу, если она еще не существует."""
+        with sqlite3.connect(self.db_path) as conn:
+            cursor = conn.cursor()
+            cursor.execute("""
+                CREATE TABLE IF NOT EXISTS posts (
+                    message_id INTEGER PRIMARY KEY,
+                    channel_id INTEGER, 
+                    text TEXT,
+                    timestamp DATETIME
+                )
+            """)
+            conn.commit()
+            logger.info('База данных создана')
+
+    def save_post(self, msg_id: int, chat_id: int, text: Optional[str], chat_name: str) -> bool:
+        """
+        Сохраняет пост в базу. 
+        Возвращает True, если сохранен, и False, если такой ID уже есть.
+        """
+        try:
+            with sqlite3.connect(self.db_path) as conn:
+                cursor = conn.cursor()
+                cursor.execute(
+                    "INSERT INTO posts (message_id, channel_id, text, timestamp) VALUES (?, ?, ?, ?)",
+                    (msg_id, chat_id, text, datetime.now())
+                )
+                logger.info(f'Пост от {chat_name} сохранён в базу данных')
+                conn.commit()
+                return True
+        except sqlite3.IntegrityError:
+            logger.info('Пост уже есть в базе данных')
+            # Сработает, если message_id уже существует (PRIMARY KEY)
+            return False
+        
+    def is_post_sent(self, msg_id: int) -> bool:
+        """Проверяет, был ли этот пост уже обработан."""
+        with sqlite3.connect(self.db_path) as conn:
+            cursor = conn.cursor()
+            cursor.execute("SELECT 1 FROM posts WHERE message_id = ?", (msg_id,))
+            return cursor.fetchone() is not None
--- main.py
+++ main.py
@@ -1,13 +1,96 @@
-from aiogram import executor, Bot, Dispatcher, types
+from telethon import TelegramClient, events
+from telethon.tl.patched import Message
 from dotenv import load_dotenv
+import asyncio
 import os
+import sys
+from typing import Final
+import logging
+import subprocess
+
+
+from database_manager import DatabaseManager
+
+LOG_FORMAT: Final[str] = "%(asctime)s [%(name)s] %(levelname)s: %(message)s"
+
+logging.basicConfig(
+    level=logging.INFO,
+    format=LOG_FORMAT,
+    handlers=[
+        logging.FileHandler("bot.log", encoding="utf-8"),
+        logging.StreamHandler(sys.stdout)
+    ]
+)
+
+logger = logging.getLogger(__name__)
 
 load_dotenv()
-TOKEN_API = os.getenv("TOKEN")
+API_KEY: Final[int] = int(os.getenv("API_KEY"))
+API_HASH: Final[str] = os.getenv("API_HASH")
+client = TelegramClient('my_session', API_KEY, API_HASH)
+
+chats: list[str] = ['@mcrepostworld', "@O_Arestovich_official", "@kontext_channel"]
 
 
-bot = Bot(token=TOKEN_API)
-dp = Dispatcher(bot)
+
+def send_sms(phone: str, text: str) -> None:
+    """Отправляет СМС через Termux API."""
+    try:
+        
+        subprocess.run(["termux-sms-send", "-n", phone, text], check=True)
+        logger.info(f"СМС успешно отправлено на номер {phone}")
+    except Exception as e:
+        logger.error(f"Ошибка при отправке СМС: {e}")
+
+@client.on(events.NewMessage(chats=chats))
+async def handler(event: events.NewMessage.Event) -> None:
+    logger.info("Новое сообщение получено")
+    db = DatabaseManager() # Можно вынести инициализацию db выше
+    
+    msg: Message = event.message
+    
+    # 1. Проверяем, есть ли текст
+    if not msg.text:
+        return
+
+    # 2. Проверяем в базе, не обрабатывали ли мы этот пост
+    if db.is_post_sent(msg.id):
+        return
+
+    # 3. Сохраняем в базу
+    if db.save_post(msg.id, msg.chat_id, msg.text):
+        logger.info(f"Новый пост обнаружен (ID: {msg.id}). Отправляю СМС...")
+        
+        # 4. Отправляем СМС (номер возьми из .env)
+        friend_phone: str = os.getenv("FRIEND_PHONE", "")
+        send_sms(friend_phone, msg.text)
+
+async def main() -> None:
+    await client.start()
+    logger.info('Бот запущен')
+
+    db = DatabaseManager()
+    for chat in chats:
+        posts: list[Message]= await client.get_messages(chat, limit=3)
+        for post in posts:
+            if post.text and not db.is_post_sent(post.id):
+                db.save_post(post.id, post.chat_id, post.text, post.chat.title)
+    
+    logger.info('Бот ожидает новые сообщения...')
+
+    await client.run_until_disconnected()
+
+
 
 if __name__ == "__main__":
-    ...
+    try:
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        with client:
+            client.loop.run_until_complete(main())
+    except KeyboardInterrupt:
+        logger.info("Бот остановлен пользователем")
+    except Exception as e:
+        logger.critical(f"Бот упал с ошибкой: {e}", exc_info=True)
+    finally:
+        logger.info("Сессия закрыта. Программа завершена.")
