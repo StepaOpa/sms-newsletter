350acb37750aeeba013c4096dbf35e4641b4b3da
StepaOpa <stepankock@yandex.ru>
2026-02-16 19:48:24 +0500

    stable version of the code

--- database_manager.py
+++ database_manager.py
@@ -1,15 +1,16 @@
-from typing import Optional, Final
+import logging
 import sqlite3
 from datetime import datetime
-import logging
+from typing import Final, Optional
 
 logger = logging.getLogger(__name__)
+
 
 class DatabaseManager:
     def __init__(self, db_path: str = "news_archive.db") -> None:
         self.db_path: Final[str] = db_path
         self._create_table()
-        logger.info('База данных инициализирована')
+        logger.info("База данных инициализирована")
 
     def _create_table(self) -> None:
         """Создает таблицу, если она еще не существует."""
@@ -24,11 +25,13 @@ class DatabaseManager:
                 )
             """)
             conn.commit()
-            logger.info('База данных создана')
+            logger.info("База данных создана")
 
-    def save_post(self, msg_id: int, chat_id: int, text: Optional[str], chat_name: str) -> bool:
+    def save_post(
+        self, msg_id: int, chat_id: int, text: Optional[str], chat_name: str
+    ) -> bool:
         """
-        Сохраняет пост в базу. 
+        Сохраняет пост в базу.
         Возвращает True, если сохранен, и False, если такой ID уже есть.
         """
         try:
@@ -36,16 +39,16 @@ class DatabaseManager:
                 cursor = conn.cursor()
                 cursor.execute(
                     "INSERT INTO posts (message_id, channel_id, text, timestamp) VALUES (?, ?, ?, ?)",
-                    (msg_id, chat_id, text, datetime.now())
+                    (msg_id, chat_id, text, datetime.now()),
                 )
-                logger.info(f'Пост от {chat_name} сохранён в базу данных')
+                logger.info(f"Пост от {chat_name} сохранён в базу данных")
                 conn.commit()
                 return True
         except sqlite3.IntegrityError:
-            logger.info('Пост уже есть в базе данных')
+            logger.info("Пост уже есть в базе данных")
             # Сработает, если message_id уже существует (PRIMARY KEY)
             return False
-        
+
     def is_post_sent(self, msg_id: int) -> bool:
         """Проверяет, был ли этот пост уже обработан."""
         with sqlite3.connect(self.db_path) as conn:
--- main.py
+++ main.py
@@ -1,15 +1,16 @@
-from telethon import TelegramClient, events
-from telethon.tl.patched import Message
-from dotenv import load_dotenv
 import asyncio
+import logging
 import os
+import subprocess
 import sys
 from typing import Final
-import logging
-import subprocess
 
-from database_manager import DatabaseManager
+from dotenv import load_dotenv
+from telethon import TelegramClient, events
+from telethon.tl.patched import Message
+
 from audio_compressor import AudioCompressor, AudioExportSettings
+from database_manager import DatabaseManager
 
 LOG_FORMAT: Final[str] = "%(asctime)s [%(name)s] %(levelname)s: %(message)s"
 
@@ -18,8 +19,8 @@ logging.basicConfig(
     format=LOG_FORMAT,
     handlers=[
         logging.FileHandler("bot.log", encoding="utf-8"),
-        logging.StreamHandler(sys.stdout)
-    ]
+        logging.StreamHandler(sys.stdout),
+    ],
 )
 
 logger = logging.getLogger(__name__)
@@ -27,11 +28,10 @@ logger = logging.getLogger(__name__)
 load_dotenv()
 API_KEY: Final[int] = int(os.getenv("API_KEY"))
 API_HASH: Final[str] = os.getenv("API_HASH")
-friends_phone = os.getenv('FRIENDS_PHONE')
-client = TelegramClient('my_session', API_KEY, API_HASH)
+friends_phone = os.getenv("FRIENDS_PHONE")
+client = TelegramClient("my_session", API_KEY, API_HASH)
 
-chats: list[str] = ['@mcrepostworld', "@kontext_channel", '@brokendance', '@vneshpol']
-
+chats: list[str] = ["@mcrepostworld", "@kontext_channel", "@brokendance", "@vneshpol"]
 
 
 def send_sms(phone: str, text: str) -> None:
@@ -42,57 +42,61 @@ def send_sms(phone: str, text: str) -> None:
     except Exception as e:
         logger.error(f"Ошибка при отправке СМС: {e}")
 
+
 @client.on(events.NewMessage(chats=chats))
 async def handler(event: events.NewMessage.Event) -> None:
     logger.info("Новое сообщение получено")
     db = DatabaseManager()
-    
+
     msg: Message = event.message
 
     if db.is_post_sent(msg.id):
         return
-    
+
     if db.save_post(msg.id, msg.chat_id, msg.text):
         logger.info(f"Новый пост обнаружен (ID: {msg.id}). Отправляю СМС...")
         send_sms(friends_phone, msg.text)
 
+
 async def main() -> None:
     await client.start()
-    logger.info('Бот запущен')
+    logger.info("Бот запущен")
 
     db = DatabaseManager()
     for chat in chats:
         # Получаем информацию о чате
         chat_info = await client.get_entity(chat)
-        
+
         # Получаем название канала
         channel_name = chat_info.title if chat_info.title else "Без названия"
 
         posts: list[Message] = await client.get_messages(chat, limit=3)
         for post in posts:
             if post.text and not db.is_post_sent(post.id):
-                send_sms(friends_phone, f'[{channel_name}] {post.text}')
+                send_sms(friends_phone, f"[{channel_name}] {post.text}")
                 db.save_post(post.id, post.chat_id, post.text, channel_name)
 
-    logger.info('Бот ожидает новые сообщения...')
+    logger.info("Бот ожидает новые сообщения...")
 
     await client.run_until_disconnected()
 
 
-
-if __name__ == "__main__":
+def test_audio_compressor() -> None:
     filename = "Red_Hot_Chili_Peppers_-_Otherside_76785243.mp3"
     default_audio_export_settings = AudioExportSettings()
     audio_compressor = AudioCompressor(default_audio_export_settings)
     audio_compressor.compress_audio(filename)
-    # try:
-    #     loop = asyncio.new_event_loop()
-    #     asyncio.set_event_loop(loop)
-    #     with client:
-    #         client.loop.run_until_complete(main())
-    # except KeyboardInterrupt:
-    #     logger.info("Бот остановлен пользователем")
-    # except Exception as e:
-    #     logger.critical(f"Бот упал с ошибкой: {e}", exc_info=True)
-    # finally:
-    #     logger.info("Сессия закрыта. Программа завершена.")
+
+
+if __name__ == "__main__":
+    try:
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        with client:
+            client.loop.run_until_complete(main())
+    except KeyboardInterrupt:
+        logger.info("Бот остановлен пользователем")
+    except Exception as e:
+        logger.critical(f"Бот упал с ошибкой: {e}", exc_info=True)
+    finally:
+        logger.info("Сессия закрыта. Программа завершена.")
